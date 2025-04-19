509ce2c788b5ba97e0b6c65b33862d91f1ea749d
crtmss <nicklol225@gmail.com>
2025-04-19 10:29:54 +0200

    Update turn.js

--- game/turn.js
+++ game/turn.js
@@ -3,7 +3,7 @@ import { saveGameState } from './game-state.js';
 let currentTurn = 'player_1';
 
 export function endTurn() {
-  currentTurn = currentTurn === 'player_1' ? 'player_2' : 'player_1';
-  document.getElementById('turnInfo').textContent = `Turn: ${currentTurn}`;
-  saveGameState();
+  const state = getState();
+  state.turn = state.turn === 'player_1' ? 'player_2' : 'player_1';
+  await updateState(state)
 }
