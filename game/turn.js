cb22abe811c8522bd02ebeec6278bc35dd5d718d
crtmss <nicklol225@gmail.com>
2025-04-19 10:15:22 +0200

    Update turn.js

--- game/turn.js
+++ game/turn.js
@@ -1,7 +1,9 @@
+import { saveGameState } from './game-state.js';
+
 let currentTurn = 'player_1';
 
 export function endTurn() {
   currentTurn = currentTurn === 'player_1' ? 'player_2' : 'player_1';
   document.getElementById('turnInfo').textContent = `Turn: ${currentTurn}`;
+  saveGameState();
 }
-
