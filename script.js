4a63468fbd632b4f858a0f1d1c23caf1cf978dc4
crtmss <nicklol225@gmail.com>
2025-04-19 09:19:14 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+import './lib/supabase.js';
+import { initMap } from './game/map.js';
+import { initLobby, createLobby, joinLobby } from './game/lobby.js';
+import { endTurn } from './game/turn.js';
+import { performAction } from './game/units.js';
+
+window.createLobby = createLobby;
+window.joinLobby = joinLobby;
+window.endTurn = endTurn;
+window.performAction = performAction;
+
+initLobby();
+initMap();
