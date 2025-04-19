e988681153213195563db73d300e0fb2741393ba
crtmss <nicklol225@gmail.com>
2025-04-19 09:21:56 +0200

    Update map.js

--- game/map.js
+++ game/map.js
@@ -1,1 +1,19 @@
+import { getHexCoordinates } from '../lib/hex.js';
+
+export let map = [];
+
+export function initMap() {
+  const canvas = document.getElementById('gameCanvas');
+  const ctx = canvas.getContext('2d');
+  const size = 20;
+
+  for (let y = 0; y < 25; y++) {
+    for (let x = 0; x < 25; x++) {
+      const { x: px, y: py } = getHexCoordinates(x, y, size);
+      ctx.beginPath();
+      ctx.arc(px, py, size, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+  }
+}
 
