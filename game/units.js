01309deaf30e62bee44ce39ac75693dc790be4a6
crtmss <nicklol225@gmail.com>
2025-04-19 09:22:58 +0200

    Update units.js

--- game/units.js
+++ game/units.js
@@ -1,1 +1,9 @@
+export let units = [
+  { id: 1, type: 'infantry', x: 2, y: 2, hp: 5, owner: 'player_1' },
+  { id: 2, type: 'archer', x: 20, y: 20, hp: 5, owner: 'player_2' }
+];
+
+export function performAction() {
+  console.log('Action executed');
+}
 
