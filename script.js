609ac1377025845faad86597eac88c0dec812f7c
crtmss <nicklol225@gmail.com>
2025-04-21 20:26:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ import { initLobby, createLobby, joinLobby } from './game/lobby.js';
 import { endTurn } from './game/turn.js';
 import { performAction } from './game/units.js';
 
+// Attach functions to window for inline HTML event handlers
 window.createLobby = createLobby;
 window.joinLobby = joinLobby;
 window.endTurn = endTurn;
 window.performAction = performAction;
 
+// Call initLobby after the DOM is fully loaded
 window.addEventListener('DOMContentLoaded', () => {
   initLobby();
 });
